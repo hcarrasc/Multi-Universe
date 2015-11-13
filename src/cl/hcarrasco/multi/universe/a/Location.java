aab8107096ce1670a24eb4130f7cc139564de339
Hector Carrasco <mail@hcarrasco.cl>
2015-11-13 15:10:13 -0300

    addign location attribute to particles

---
+++ src/cl/hcarrasco/multi/universe/a/Location.java
@@ -0,0 +1,9 @@
+package cl.hcarrasco.multi.universe.a;
+
+public class Location {
+
+	public double x = 0; // width component, expressed in KM
+	public double y = 0; // height component, expressed in KM
+	public double z = 0; // depth component, expressed in KM
+	
+}
